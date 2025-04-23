b24b2a344841490391f6a6b79e5c1062f476bce8
Thebeatkicks <000henrik@gmail.com>
2025-04-23 22:35:06 +0200

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,32 @@ import Portfolio from "./Components/Portfolio.jsx";
 function App() {
   return (
     <>
-    <div>
+    <div className="header">
       <h1 className="title">My Portfolio</h1>
       <p className="text">This is my portfolio</p>
     </div>
     <div className="container-row">
-      <Portfolio></Portfolio> 
-      <Portfolio></Portfolio> 
-      <Portfolio></Portfolio> 
+      <Portfolio  title="Test"
+                  skills = "HTML, CSS"
+                  summary = "a practice project to learn JS"
+                  learned = "how to use React"
+                  challenges = "how to use React Router" 
+                  link = "http://github.com"> 
+      </Portfolio> 
+      <Portfolio  title="Test"
+                  skills = "HTML, CSS, JS, REACT"
+                  summary = "a practice project to learn JS"
+                  learned = "how to use React"
+                  challenges = "how to use React Router" 
+                  link = "http://github.com"> 
+        </Portfolio> 
+        <Portfolio  title="Test"
+                  skills = "HTML, CSS, JS, REACT"
+                  summary = "a practice project to learn JS"
+                  learned = "how to use React"
+                  challenges = "how to use React Router" 
+                  link = "http://github.com"> 
+        </Portfolio> 
     </div>
     </>
   );
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,19 +1,19 @@
 import screen from "../assets/react.svg";
 import './portfolio.css';
 
-function Portfolio() {
+function Portfolio(props) {
+  
 return (
     <>
     
       <div className="container">
-        <h2 className="card-title">Test page</h2>
+        <h2 className="card-title">{props.title}</h2>
         <img src={screen} className="img" alt="Placeholder" />
-        <a href="https://github.com/thebeatkicks" className="link">Github link</a>
-        <p className="card-text">For this project I used: HTML, CSS, JS, REACT</p>
-        <p className="card-text">This project is xxx</p>
-        <p className="card-text">I learned xxx</p>
-        <p className="card-text">This project is xxx</p>
-        <p className="card-text">The biggest challenge was xxx</p>
+        <a href={props.link} className="link">Github link</a>
+        <p className="card-text">For this project I used: {props.skills}</p>
+        <p className="card-text">This project is {props.summary}</p>
+        <p className="card-text">I learned {props.learned}</p>
+        <p className="card-text">The biggest challenge was {props.challenges}</p>
       </div>
   
     </>
